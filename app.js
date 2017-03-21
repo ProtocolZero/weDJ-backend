5c6b656e22b203dedf16fbba3946c5599e458f9c
John Shepler <john.sheplerjr@gmail.com>
2017-03-21 11:43:55 -0600

    add dependencies for passport

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const helmet = require('helmet');
 const express = require('express');
 
 const playlistSong = require('./routes/playlist_song');
-
+const playlist = require('./routes/playlist');
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+
 
 // Middleware
 app.use(logger('dev'));
@@ -18,6 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
 app.use('/playlist_song', playlistSong);
+app.use('/playlist', playlist);
 
 // Listening port
 app.listen(PORT, () => {
