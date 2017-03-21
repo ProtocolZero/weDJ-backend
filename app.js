240da7e862caf00c0994545b389c0c6e9d971806
John Shepler <john.sheplerjr@gmail.com>
2017-03-21 13:56:34 -0600

    setup of facebook passport

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const bodyParser = require('body-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
 const express = require('express');
-
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const expressSession = require('express-session');
+const passport = require('./passport');
+const facebook = require('./routes/facebook')
 const playlistSong = require('./routes/playlist_song');
 
 const PORT = process.env.PORT || 3000;
@@ -15,9 +19,17 @@ app.use(logger('dev'));
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cookieParser());
+app.use(session({
+  secret: 'mouse dog',
+  saveUninitialized: true,
+  resave: false
+}));
+app.use(passport.initilaize());
+app.use(passport.session());
 // Routes
 app.use('/playlist_song', playlistSong);
+app.use('/facebook', facebook);
 
 // Listening port
 app.listen(PORT, () => {
