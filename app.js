a48a17b6bf3a4b95f5a418f95f0fd5e8f4229877
DariaDesiderata <daria.calwell@gmail.com>
2017-03-21 19:48:09 -0600

    add comments routes

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const playlist = require('./routes/playlist');
 const facebook = require('./routes/facebook');
 const playlistSong = require('./routes/playlist_song');
 const role = require('./routes/role');
+const comment = require('./routes/comments');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -42,6 +43,7 @@ app.use('/playlist_song', playlistSong);
 app.use('/song', song);
 app.use('/role', role);
 app.use('/user', user);
+app.use('/comment', comment)
 
 // Listening port
 app.listen(PORT, () => {
